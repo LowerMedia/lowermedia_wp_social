41875a460de190d52bee459744dc388f442caa6f
Pete Lower <pete.lower@gmail.com>
2013-05-10 10:00:05 -0500

    Version 1: Addition adding more icons, made some comment edits
    Email
    Rss
    SoundCloud
    Blogger
    Reverbnation
    BandCamp

--- lowermedia-wp-social.php
+++ lowermedia-wp-social.php
@@ -1,4 +1,4 @@
-<?php
+at <?php
 /*
 Plugin Name: LowerMedia WP Social
 Plugin URI: http://lowermedia.net
@@ -13,53 +13,50 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 /*############################################################################################
 #
 #   REGISTER PLUGIN STYLES
-#   //These functions enques and registers the plugin stylesheet
+#   //These functions enque and registers the plugin stylesheet
+#	 //Register with hook 'wp_enqueue_scripts', which can be used for front end CSS and JavaScript (From Codex)
 */
-
-/**
- * Register with hook 'wp_enqueue_scripts', which can be used for front end CSS and JavaScript
- */
-add_action( 'wp_enqueue_scripts', 'lowermedia_add_my_stylesheet' );
-
-/**
- * Enqueue plugin style-file
- */
-function lowermedia_add_my_stylesheet() {
-    // Respects SSL, Style.css is relative to the current file
-    wp_register_style( 'lowermedia-style', plugins_url('style.css', __FILE__) );
-    wp_enqueue_style( 'lowermedia-style' );
-}
+	add_action( 'wp_enqueue_scripts', 'lowermedia_add_my_stylesheet' );
+	
+	/**
+	 * Enqueue plugin style-file
+	 */
+	function lowermedia_add_my_stylesheet() {
+	    // Respects SSL, Style.css is relative to the current file
+	    wp_register_style( 'lowermedia-style', plugins_url('style.css', __FILE__) );
+	    wp_enqueue_style( 'lowermedia-style' );
+	}
 
 /*############################################################################################
 #
 #   REGISTER WDIGETS
 #   //This function creates and registers the social media icon holder widget
 */
-function lowermedia_wp_social_init() {
-    register_sidebar( array(
-		'name' => 'Social Media Area',
-		'id' => 'lowermedia_wp_social_widget_area',
-		'before_widget' => '<div id="lowermedia-wp-social-wrap" style="position:fixed;">',
-		'after_widget' => '</div>',
-		'before_title' => '<h2 class="rounded">',
-		'after_title' => '</h2>',
-	) );
-}
-add_action('widgets_init', 'lowermedia_wp_social_init');
+	function lowermedia_wp_social_init() {
+	    register_sidebar( array(
+			'name' => 'Social Media Area',
+			'id' => 'lowermedia_wp_social_widget_area',
+			'before_widget' => '<div id="lowermedia-wp-social-wrap" style="position:fixed;">',
+			'after_widget' => '</div>',
+			'before_title' => '<h2 class="rounded">',
+			'after_title' => '</h2>',
+		) );
+	}
+	add_action('widgets_init', 'lowermedia_wp_social_init');
 
 
 /*############################################################################################
 #
 #   ADD POST CONTENT
-#   //This function adds information to the end of the post,,,,,  && !is_home()
+#   //This function adds information to the end of the post
 */
 
-function lowermedia_add_wp_social($output) {
-		$output = dynamic_sidebar('lowermedia_wp_social_widget_area');
-		return $output;
-}
-
-add_filter('wp_head', 'lowermedia_add_wp_social', 1000);//1000 is there to make sure this is loaded very lastly to the head
+	function lowermedia_add_wp_social($output) {
+			$output = dynamic_sidebar('lowermedia_wp_social_widget_area');
+			return $output;
+	}
+	
+	add_filter('wp_head', 'lowermedia_add_wp_social', 1000);//1000 is used to make sure this is loaded very lastly to the head
 
 
 /*############################################################################################
